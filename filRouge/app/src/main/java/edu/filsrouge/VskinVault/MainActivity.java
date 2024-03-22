7e800299d7618b29b5d3034e445a0f4c766b2a36
Tom DA COSTA <tom.da-costa@etu.unice.fr>
2024-03-22 13:32:26 +0100

    connexion view

--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
@@ -2,7 +2,10 @@ package edu.filsrouge.VskinVault;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
