d890eed5913f1f0effe3f6a2eb23c0549b137934
tronconi tristan <tristan.tronconi@etu.univ-cotedazur.fr>
2024-03-29 14:02:40 +0100

    activités fonctionnelles et changement de vue

--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/CommandActivity.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/CommandActivity.java
@@ -1,4 +1,42 @@
 package edu.filsrouge.VskinVault;
 
-public class CommandActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CommandActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command);
+
+        String[] categories = getResources().getStringArray(R.array.category);
+        TextView[] titles = new TextView[6];
+        titles[0] = findViewById(R.id.centerTitle);
+        titles[0].setText(categories[0]);
+        titles[0].setTextSize(20);
+        for (int i = 1; i <categories.length ; i++) {
+            titles[i] = findViewById(getResources().getIdentifier("textView"+i, "id", getPackageName()));
+            titles[i].setText(categories[i]);
+        }
+
+        TextView button = findViewById(R.id.confirm_button);
+        button.setText(R.string.validate_command);
+
+
+        //retour au menu principal
+        TextView menu = findViewById(R.id.navTitle);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créez une nouvelle Intent pour ouvrir CommandActivity
+                Intent intent = new Intent(CommandActivity.this, MainActivity.class);
+
+                // Démarrez l'activité
+                startActivity(intent);
+            }
+        });/**/
+    }
 }
--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
@@ -2,9 +2,13 @@ package edu.filsrouge.VskinVault;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,18 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] categories = getResources().getStringArray(R.array.category);
+        ///nav connexion
+        // nav pannier
+        ImageView[] images = new ImageView[6];
         LinearLayout[] layouts = new LinearLayout[6];
         TextView[] titles = new TextView[6];
 
         for(int i = 0; i < categories.length; i++) {
-            layouts[i] = findViewById(getResources().getIdentifier("navLayout" + i, "id", getPackageName()));
+            layouts[i]= findViewById(getResources().getIdentifier("navLayout"+i, "id", getPackageName()));
+            //images-categories[i] = (ImageView) layouts[i].getChildAt(0);
+            //images[i].setImageResource();
+            titles[i] = (TextView) layouts[i].getChildAt(1);
+            titles[i].setText(categories[i]);
+        }/**/
 
-            if (titles[i] != null) {
-                titles[i].setText(categories[i]);
-            } else {
-                Log.e("MainActivity", "TextView with id 'navLayout" + i + "' is not found.");
+
+        ImageButton button = findViewById(R.id.profil);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créez une nouvelle Intent pour ouvrir CommandActivity
+                Intent intent = new Intent(MainActivity.this, CommandActivity.class);
+
+                // Démarrez l'activité
+                startActivity(intent);
             }
-        }
-
+        });/**/
     }
 }
