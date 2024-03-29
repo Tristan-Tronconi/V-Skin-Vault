624e82aa3a0053d234854e6c2a4a49528de509fb
tronconi tristan <tristan.tronconi@etu.univ-cotedazur.fr>
2024-03-29 15:27:56 +0100

    filter

--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/CommandActivity.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/CommandActivity.java
@@ -1,10 +1,18 @@
 package edu.filsrouge.VskinVault;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CommandActivity extends AppCompatActivity {
     @Override
@@ -12,18 +20,52 @@ public class CommandActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_command);
 
+
+        InputFilter filter = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (Character.toString(source.charAt(i)).matches("[\\t\\n$=*+(){}/?!:;,\"<>%-]")) {return "";}
+                }
+                return null;
+            }
+        };
+
         String[] categories = getResources().getStringArray(R.array.category);
         TextView[] titles = new TextView[6];
+        TextInputLayout[] textInputLayout = new TextInputLayout[6];
+        TextInputEditText[] textInputEditTexts = new TextInputEditText[6];
         titles[0] = findViewById(R.id.centerTitle);
         titles[0].setText(categories[0]);
         titles[0].setTextSize(20);
         for (int i = 1; i <categories.length ; i++) {
             titles[i] = findViewById(getResources().getIdentifier("textView"+i, "id", getPackageName()));
             titles[i].setText(categories[i]);
+            textInputLayout[i] = findViewById(getResources().getIdentifier("textInputLayout"+i, "id", getPackageName()));
+            textInputEditTexts[i] = findViewById(getResources().getIdentifier("value"+i, "id", getPackageName()));
+            textInputEditTexts[i].setFilters(new InputFilter[]{filter});
+
         }
 
         TextView button = findViewById(R.id.confirm_button);
         button.setText(R.string.validate_command);
+
+        //search bar
+        TextInputLayout searchLayout = findViewById(R.id.searchBarLayout);
+        searchLayout.setHintAnimationEnabled(true);
+        TextInputEditText search = findViewById(R.id.searchBar);
+        search.setFilters(new InputFilter[]{filter});
+        //quitter la barre de recherche
+        findViewById(android.R.id.content).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (!(v instanceof TextInputEditText)) {
+                    search.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
 
 
         //retour au menu principal
--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/MainActivity.java
@@ -2,15 +2,23 @@ package edu.filsrouge.VskinVault;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.lang.reflect.Array;
 
@@ -37,9 +45,39 @@ public class MainActivity extends AppCompatActivity {
             titles[i].setText(categories[i]);
         }/**/
 
+        //search bar
+        TextInputLayout searchLayout = findViewById(R.id.searchBarLayout);
+        searchLayout.setHintAnimationEnabled(true);
+        TextInputEditText search = findViewById(R.id.searchBar);
+        InputFilter filter = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (Character.toString(source.charAt(i)).matches("[\\t\\n$=*+(){}/?!:;,\"<>%-]")) {
+                        return "";
+                    }
+                }
+                return null;
+            }
 
+        };
+        search.setFilters(new InputFilter[]{filter});
+        //quitter la barre de recherche
+        findViewById(android.R.id.content).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (!(v instanceof TextInputEditText)) {
+                    search.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
+
+
+
+        //retour au menu principal
         ImageButton button = findViewById(R.id.profil);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
