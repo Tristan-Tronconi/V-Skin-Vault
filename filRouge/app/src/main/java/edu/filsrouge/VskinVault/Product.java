4195d67ce2f760d55b62dd8e2e0d2e1f2740931e
hemeret sheun <hs201030@campus.unice.fr@S655P06L.campus.unice.fr>
2024-04-02 13:35:58 +0200

    Requetes fonctionnelles

---
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/Product.java
@@ -0,0 +1,2 @@
+package edu.filsrouge.VskinVault;public class Product {
+}
--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/RequestHandler.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/RequestHandler.java
@@ -1,52 +1,65 @@
 package edu.filsrouge.VskinVault;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
-public class RequestHandler {
-    public HashMap<String, String>[] searchCosmetics(HashMap<String, String> params) {
-        HashMap<String,String>[] response = null;
+public class APIHandler {
+    public Product[] searchCosmetics(HashMap<String, String> params) {
+
+        Product[] response = null;
         String reqUrl="https://fortnite-api.com/v2/cosmetics/br/search/all";
 
-        for (String key : params.keySet()) {
-            reqUrl += "?"+key+"="+params.get(key);
+        for (int i = 0; i < params.size(); i++) {
+            String key = (String) params.keySet().toArray()[i];
+            String value = params.get(key);
+            if(i == 0)
+                reqUrl += "?";
+            else
+                reqUrl += "&";
+            reqUrl += key + "=" + value;
         }
 
         try {
+            System.out.println("Request URL: " + reqUrl); //TODO remove
+
             URL url = new URL(reqUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
 
             InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToMaps(in);
+            response = convertStreamToProducts(in);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
         return response;
     }
 
-    public HashMap<String, String>[]  searchCosmeticsByName(String name) {
+    public Product[]  searchCosmeticsByName(String name) {
         return searchCosmetics(new HashMap<String, String>() {{
             put("name", name);
         }});
     }
 
-    public HashMap<String, String>[]  searchCosmeticsByType(String type) {
+    public Product[] searchCosmeticsByType(String type) {
         return searchCosmetics(new HashMap<String, String>() {{
             put("type", type);
         }});
     }
 
-    public HashMap<String, String>[]  getCosmeticInfo(String id) {
-        HashMap<String,String>[] response = null;
+    public Product[]  getCosmeticInfo(String id) {
+        Product[] response = null;
         String reqUrl="https://fortnite-api.com/v2/cosmetics/br/"+id;
         try {
             URL url = new URL(reqUrl);
@@ -54,27 +67,62 @@ public class RequestHandler {
             conn.setRequestMethod("GET");
 
             InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToMaps(in);
+            response = convertStreamToProducts(in);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return response;
     }
 
-    private HashMap<String, String>[] convertStreamToMaps(InputStream in) {
+    private String convertStreamToString(InputStream in) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         String line;
         try {
             while ((line = reader.readLine()) != null) {
-                result.append(line);
+                sb.append(line).append('\n');
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return sb.toString();
+    }
 
-        String json = result.toString();
-        System.out.println(json);
-        return new HashMap[]{new HashMap<String, String>()};
+    private Product[] convertStreamToProducts(InputStream in) {
+        String json = convertStreamToString(in);
+        try {
+            // Create ObjectMapper instance
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Read JSON as tree
+            JsonNode rootNode = objectMapper.readTree(json);
+
+            // Get the "data" array from the JSON
+            JsonNode dataArray = rootNode.get("data");
+
+            // If "data" array exists and is an array
+            if (dataArray != null && dataArray.isArray()) {
+                // Iterate through the array and deserialize each product
+                List<Product> productList = new ArrayList<>();
+                Iterator<JsonNode> iterator = dataArray.elements();
+                while (iterator.hasNext()) {
+                    JsonNode productNode = iterator.next();
+                    Product product = objectMapper.treeToValue(productNode, Product.class);
+                    productList.add(product);
+                }
+                return productList.toArray(new Product[0]);
+            } else {
+                System.out.println("No data array found in the JSON.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
