cc9e4aa6a8ef05a53f7656c8d2964c6848d4e5c3
tronconi tristan <tristan.tronconi@etu.univ-cotedazur.fr>
2024-04-03 16:19:46 +0200

    animator

--- filRouge/app/src/main/java/edu/filsrouge/VskinVault/PanierAdapter.java
+++ filRouge/app/src/main/java/edu/filsrouge/VskinVault/PanierAdapter.java
@@ -1,15 +1,20 @@
 package edu.filsrouge.VskinVault;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import java.text.DecimalFormat;
 import java.util.List;
@@ -47,32 +52,22 @@ public class PanierAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ConstraintLayout layoutItem;
 
-        //(1) : Réutilisation des layouts impossible (mémorisation des valeurs du ratingBar)
-        //layoutItem = (ConstraintLayout) (convertView == null ? mInflater.inflate(R.layout.character_layout, parent, false) : convertView);
         layoutItem = (ConstraintLayout) mInflater.inflate(R.layout.panier_product_layout, parent, false);
 
-
-        //(2) : Récupération des éléments
+        // Récupération des éléments
         ImageView picture = layoutItem.findViewById(R.id.image);
         TextView name = layoutItem.findViewById(R.id.name);
         RatingBar ratingBar = layoutItem.findViewById(R.id.ratingBar);
         TextView description = layoutItem.findViewById(R.id.desc);
         TextView price = layoutItem.findViewById(R.id.price);
 
-
-
-
-
-        //(3) : Renseignement des valeurs
+        // Renseignement des valeurs
         name.setText(products.get(position).getName());
         description.setText(products.get(position).getDescription());
-        //price.setText(products.get(position).getPrice());
         price.setText("price : XXXX €");
-        //picture.setImageResource(products.get(position).getIcon());
         picture.setImageResource(R.drawable.ic_launcher_background);
-
-        Random random = new Random();
-        ratingBar.setRating(random.nextInt(5) + 1);
+        ratingBar.setRating(new Random().nextInt(5) + 1);
+        //ratingBar.setRating(products.get(position).getRating());
 
         ratingBar.setOnRatingBarChangeListener((ratingBar1, value, b) -> {
             Log.d(TAG, " seekbar change to " + value);
@@ -80,10 +75,36 @@ public class PanierAdapter extends BaseAdapter {
         });
 
         layoutItem.setOnClickListener( click -> callBackActivity.onClicItem(position));
-        //On retourne l'item créé.
+        // Création de l'animation
+        ObjectAnimator animator = ObjectAnimator.ofFloat(layoutItem, "translationY", 3000f, 0f);
+        animator.setDuration(1000); // Durée de l'animation en millisecondes
+        animator.setStartDelay(position * 200);
+        animator.start();
+
+
+        ListView listView = (ListView) parent;
+        listView.setDivider(null);
+        layoutItem.setVisibility(View.INVISIBLE);
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                // Désactiver le divider
+                layoutItem.setVisibility(View.VISIBLE);
+                listView.setDivider(null);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // Réactiver le divider
+                listView.setDivider(ContextCompat.getDrawable(callBackActivity.getContext(), android.R.drawable.divider_horizontal_dark));
+            }
+        });
+
+        // On retourne l'item créé.
         return layoutItem;
     }
-
 
 }
 
